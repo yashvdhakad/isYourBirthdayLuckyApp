d999f780ce00d20f51ac01294a6dec954a2f1c53
Yash <yashvdkd@gmail.com>
2022-10-31 19:15:23 +0530

    feat: all logic done

---
+++ app.js
@@ -0,0 +1,31 @@
+const dateOfBirth = document.querySelector("#dateOfBirth");
+const luckyNumber = document.querySelector("#luckyNumber");
+const checkButton = document.querySelector("#checkButton");
+const outputBox = document.querySelector("#outputBox");
+
+function calculateSum(dob) {
+    dob = dateOfBirth.value.replaceAll("-", "");
+    let sum = 0;
+    for (i = 0; i < dob.length; i++) {
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+function compareValues(sum) {
+  if (sum % luckyNumber.value === 0) {
+    outputBox.innerText = "You are lucky!"
+} else {
+    outputBox.innerText = "You ain't lucky!"
+  }
+}
+
+checkButton.addEventListener("click", function checkBirthdayIsLucky() {
+  const dob = dateOfBirth.value;
+  if (dob && luckyNumber.value) {
+      const sum = calculateSum(dob);
+      compareValues(sum);
+  } else {
+    outputBox.innerText = "Please enter both the fields!"
+  }
+});
